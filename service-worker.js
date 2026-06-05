12d023d74e002b37d507d5c00e2f0f3e7ef40c95
danielhabted-svg <danielhabted@gmail.com>
2026-06-05 23:45:44 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "monis-beautiful-days-v3-clean";
+const CACHE_NAME = "monis-beautiful-days-final-complete-v1";
 const FILES_TO_CACHE = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))));
 self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(response => response || fetch(event.request))));
